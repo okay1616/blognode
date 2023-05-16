4e325fba07517089a7cdbee7248805dfa15b6e95
okay1616 <130121871+okay1616@users.noreply.github.com>
2023-05-16 13:51:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require('mongoose')
 const url = "mongodb://localhost:27017/First";
-const connectToMongoose = () => {
+const connectToMongoose = await () => {
     mongoose.connect(url, { })
         .then(() => console.log(`Server Connected To Mongodb`))
         .catch((error) => console.log(`${error} did not connect`));
